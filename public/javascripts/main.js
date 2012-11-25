393bd73a84f3e8ea097612080644ed460e782ac4
David Batranu <dbatranu@gmail.com>
2012-11-25 16:08:10 +0200

    Pretty step printing
    - nice step counter display;
    - saving working steps for future reference
    - improved styling and layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,6 +8,7 @@ jsapp = {
 	start: null,
 	step: null,
 	sound: null,
+	elems: {},
 	stepMap: function(){
 		var self = this;
 		var map = {}
@@ -108,9 +109,34 @@ jsapp = {
 		setInterval(function(){
 			var currentTime = self.getCurrentTime();
 			var step = stepMap[currentTime];
-			if(step) console.log(step);
+			if(step) {
+				self.showStep(step);
+				//console.log(step);
+			}
 		}, 1)
-
+	},
+	showStep: function(step){
+		var self = this;
+		if(!self.elems.span){
+			self.elems.span = jQuery('<span class="badge">10</span>');	
+		}
+		if(step != self.elems.span.text()){
+			self.elems.span.fadeIn(0);
+			self.chooseClassForCounter(step);
+			jQuery('#counter').append(self.elems.span);
+			self.elems.span.text(step);
+			self.elems.span.fadeOut('1');
+		}
+	},
+	chooseClassForCounter: function(step){
+		var self = this;
+		var toRemove = 'badge-important badge-warning badge-info';
+		var toAdd = '';
+		if(step == 1 || step == 5) toAdd = 'badge-important';
+		if(step == 2 || step == 6) toAdd = 'badge-warning';
+		if(step == 3 || step == 7) toAdd = 'badge-info';
+		self.elems.span.removeClass(toRemove);
+		self.elems.span.addClass(toAdd);
 	},
 	smDeploy: function(){
 		var self = this;
@@ -136,4 +162,7 @@ jQuery(document).ready(function(){
 	//jsapp.smDeploy();
 })
 
-//[9456, 9796, 10214, 10893, 11285, 11703, 12382, 12800, 13192, 13871, 14289, 14655, 15412, 15778, 16196, 16901, 17267, 17685]
+
+//working steps
+//[9.11, 9.47, 9.86, 10.56, 10.92, 11.32, 12.11, 12.48, 12.87, 13.62, 13.98, 14.36, 15.11, 15.47, 15.86, 16.55, 16.94, 17.34]
+//[9.1, 9.45, 9.87, 10.6, 10.95, 11.33, 12.08, 12.47, 12.88, 13.58, 13.97, 14.34, 15.08, 15.48, 15.87, 16.57, 16.96, 17.32, 18.09, 18.47, 18.85, 19.57, 19.95, 20.34, 21.08, 21.46, 21.88, 22.57, 22.96, 23.34]
