a2c92ac918173d2ffab2e40dd1527389262e45d2
David Batranu <dbatranu@gmail.com>
2013-04-13 14:07:58 +0300

    Indenting, jslint, index

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,200 +3,204 @@ if(!window.jsapp){
 }
 
 jsapp = {
-    sc: null,
-    steps: [],
-    audio: null,
-    start: null,
-    step: null,
-    sound: null,
-    elems: {},
-    stepMap: function(){
-        var self = this;
-        var map = {}
-        var s = 1;
-        var duration = self.getDuration();
-        var steps = self.steps.slice();
-        var offset = self.getOffset(steps);
-        var last_steps = steps.slice(6, steps.length);
-        while(parseInt(steps.slice(-1)) < duration){
-            var new_steps = jQuery.map(last_steps, function(o, i){
-                return self.getFloat(Big(o).plus(offset));
-            })
-            steps.push.apply(steps, new_steps);
-            last_steps = new_steps.slice();
-        }
-        for(var i = 0; i <= steps.length - 1; i++){
-            if(s == 4) s = 5;
-            if(s == 8) s = 1;
-            map[steps[i]] = s;
-            s++;
-        }
-        return map;
-    },
-    getDuration: function(){
-        var self = this;
-        if (self.sound) return self.sound.durationEstimate;
-        else if (self.audio) return self.audio.duration;
-    },
-    getFloat: function(big){
-        var self = this;
-        return parseFloat(big.toFixed(2));
-    },
-    getOffset: function(list){
-        var self = this;
-        var last = list.slice(-6, -5);
-        var first = list.slice(-list.length, -list.length + 1);
-        return self.getFloat(Big(last).minus(first));
-    },
-    restartPlay: function(){
-        var self = this;
-        if(self.sound){
-            self.sound.setPosition(0);
-            self.sound.stop();
-            self.sound.play();
-        }
-        else if(self.audio){
-            self.audio.currentTime = 0;
-            self.audio.play();
-        }
-    },
-    printSteps: function(){
-        var self = this;
-        var last = null;
-        var stepMap = self.stepMap();
-        if (self.sound) self.handleSound(stepMap);
-        else if (self.audio) self.handleAudio(stepMap);
-    },
-    getCurrentTime: function(){
-        var self = this;
-        if(self.sound) return self.sound.position;
-        if(self.audio) return self.getFloat(Big(self.audio.currentTime));
-    },
-    buildStepList: function(steptimes){
-        var self = this;
-        if(self.sound) {
-            var array = jQuery.map(steptimes, function(i, o) {
-                return parseInt(i);
-            });
-            return array;
-        };
-        if(self.audio) {
-            var array = jQuery.map(steptimes, function(i, o) {
-                return self.getFloat(i);
-            });
-            return array;
-        };
-    },
-    startPlay: function(callback){
-        var self = this;
-        if(self.sound) {
-            soundManager.play('aSound', {
-                onplay: function(){
-                    callback();
-                }
-            });
-        };
-        if(self.audio) {
-            self.audio.play();
-            callback();
-        };
-    },
-    stepDifference: function(currentTime, nextTime){
-        var self = this;
-        var difference = self.getFloat(Big(nextTime).minus(currentTime));
-        return difference
-    },
-    maxDifference: function(){
-        if(navigator.userAgent.indexOf('Chrome') != -1 ) return 100;
-        if(navigator.userAgent.indexOf('Firefox') != -1 ) return 300;
-        return 200;
-    },
-    handlePlayback: function(stepMap, callback){
-        var self = this;
-        var duration = self.getDuration();
-        var steptimes = Object.keys(stepMap);
-        var steplist = null;
-        var nextTime = null;
-        var lastTime = 0;
-        var lastStep = 0;
-        var maxDifference = self.maxDifference();
-        var queryTime = function(){
-            if (steplist == null && nextTime == null){
-                steplist = self.buildStepList(steptimes);
-                nextTime = steplist[0];
-            }
-            var currentTime = self.getCurrentTime();
-            if(self.stepDifference(currentTime, nextTime) <= maxDifference) {
-                var step = stepMap[nextTime];
-                if(step && step != lastStep) {
-                    self.showStep(step);
-                    callback(step);
-                    lastStep = step;
-                }
-                lastTime = currentTime;
-                steplist = steplist.slice(1);
-                nextTime = steplist[0];
-            }
-            if(duration > currentTime && currentTime != duration){
-                //pass
-            }
-        }
-        self.startPlay(function(){
-            setInterval(queryTime, 10);
-        });
-    },
-    showStep: function(step){
-        var self = this;
-        if(!self.elems.span){
-            self.elems.span = jQuery('<span class="badge">10</span>');
-        }
-        if(step != self.elems.span.text()){
-            //self.elems.span.fadeIn(100);
-            self.chooseClassForCounter(step);
-            jQuery('#counter').append(self.elems.span);
-            self.elems.span.text(step);
-            //self.elems.span.fadeOut('100');
-        }
-    },
-    chooseClassForCounter: function(step){
-        var self = this;
-        var toRemove = 'badge-important badge-warning badge-info';
-        var toAdd = '';
-        if(step == 1 || step == 5) toAdd = 'badge-important';
-        if(step == 2 || step == 6) toAdd = 'badge-warning';
-        if(step == 3 || step == 7) toAdd = 'badge-info';
-        self.elems.span.removeClass(toRemove);
-        self.elems.span.addClass(toAdd);
-    },
-    smDeploy: function(callback){
-        var self = this;
-        soundManager.setup({
-            url: '/swf/',
-            debugMode: false,
-            onready: function() {
-                self.sound = soundManager.createSound({
-                    id: 'aSound',
-                    url: '/files/salsalesson.mp3'
-                });
-                soundManager.load('aSound', {
-                    onload: function(){
-                        callback();
-                    }
-                });
-                
-            },
-            ontimeout: function() {
-                // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
-            }
-        });
+  sc: null,
+  steps: [],
+  audio: null,
+  start: null,
+  step: null,
+  sound: null,
+  elems: {},
+  stepMap: function(){
+    var self = this;
+    var map = {};
+    var s = 1;
+    var duration = self.getDuration();
+    var steps = self.steps.slice();
+    var offset = self.getOffset(steps);
+    var last_steps = steps.slice(6, steps.length);
+    function getFloatList(list){
+      return jQuery.map(last_steps, function(o, i){
+        return self.getFloat(Big(o).plus(offset));
+      });
     }
-}
+    while(parseInt(steps.slice(-1), 10) < duration){
+      var new_steps = getFloatList(last_steps);
+      steps.push.apply(steps, new_steps);
+      last_steps = new_steps.slice();
+    }
+    for(var i = 0; i <= steps.length - 1; i++){
+      if(s == 4) s = 5;
+      if(s == 8) s = 1;
+      map[steps[i]] = s;
+      s++;
+    }
+    return map;
+  },
+  getDuration: function(){
+    var self = this;
+    if (self.sound) return self.sound.durationEstimate;
+    else if (self.audio) return self.audio.duration;
+  },
+  getFloat: function(big){
+    var self = this;
+    return parseFloat(big.toFixed(2));
+  },
+  getOffset: function(list){
+    var self = this;
+    var last = list.slice(-6, -5);
+    var first = list.slice(-list.length, -list.length + 1);
+    return self.getFloat(Big(last).minus(first));
+  },
+  restartPlay: function(){
+    var self = this;
+    if(self.sound){
+      self.sound.setPosition(0);
+      self.sound.stop();
+      self.sound.play();
+    }
+    else if(self.audio){
+      self.audio.currentTime = 0;
+      self.audio.play();
+    }
+  },
+  printSteps: function(){
+    var self = this;
+    var last = null;
+    var stepMap = self.stepMap();
+    if (self.sound) self.handleSound(stepMap);
+    else if (self.audio) self.handleAudio(stepMap);
+  },
+  getCurrentTime: function(){
+    var self = this;
+    if(self.sound) return self.sound.position;
+    if(self.audio) return self.getFloat(Big(self.audio.currentTime));
+  },
+  buildStepList: function(steptimes){
+    var self = this;
+    var array;
+    if(self.sound) {
+      array = jQuery.map(steptimes, function(i, o) {
+        return parseInt(i, 10);
+      });
+      return array;
+    }
+    if(self.audio) {
+      array = jQuery.map(steptimes, function(i, o) {
+        return self.getFloat(i);
+      });
+      return array;
+    }
+  },
+  startPlay: function(callback){
+    var self = this;
+    if(self.sound) {
+      soundManager.play('aSound', {
+        onplay: function(){
+          callback();
+        }
+      });
+    }
+    if(self.audio) {
+      self.audio.play();
+      callback();
+    }
+  },
+  stepDifference: function(currentTime, nextTime){
+    var self = this;
+    var difference = self.getFloat(Big(nextTime).minus(currentTime));
+    return difference;
+  },
+  maxDifference: function(){
+    if(navigator.userAgent.indexOf('Chrome') != -1 ) return 100;
+    if(navigator.userAgent.indexOf('Firefox') != -1 ) return 300;
+    return 200;
+  },
+  handlePlayback: function(stepMap, callback){
+    var self = this;
+    var duration = self.getDuration();
+    var steptimes = Object.keys(stepMap);
+    var steplist = null;
+    var nextTime = null;
+    var lastTime = 0;
+    var lastStep = 0;
+    var maxDifference = self.maxDifference();
+    var queryTime = function(){
+      if (steplist === null && nextTime === null){
+        steplist = self.buildStepList(steptimes);
+        nextTime = steplist[0];
+      }
+      var currentTime = self.getCurrentTime();
+      if(self.stepDifference(currentTime, nextTime) <= maxDifference) {
+        var step = stepMap[nextTime];
+        if(step && step != lastStep) {
+          self.showStep(step);
+          callback(step);
+          lastStep = step;
+        }
+        lastTime = currentTime;
+        steplist = steplist.slice(1);
+        nextTime = steplist[0];
+      }
+      if(duration > currentTime && currentTime != duration){
+        //pass
+      }
+    };
+    self.startPlay(function(){
+      setInterval(queryTime, 10);
+    });
+  },
+  showStep: function(step){
+    var self = this;
+    if(!self.elems.span){
+      self.elems.span = jQuery('<span class="badge">10</span>');
+    }
+    if(step != self.elems.span.text()){
+      //self.elems.span.fadeIn(100);
+      self.chooseClassForCounter(step);
+      jQuery('#counter').append(self.elems.span);
+      self.elems.span.text(step);
+      //self.elems.span.fadeOut('100');
+    }
+  },
+  chooseClassForCounter: function(step){
+    var self = this;
+    var toRemove = 'badge-important badge-warning badge-info';
+    var toAdd = '';
+    if(step == 1 || step == 5) toAdd = 'badge-important';
+    if(step == 2 || step == 6) toAdd = 'badge-warning';
+    if(step == 3 || step == 7) toAdd = 'badge-info';
+    self.elems.span.removeClass(toRemove);
+    self.elems.span.addClass(toAdd);
+  },
+  smDeploy: function(callback){
+    var self = this;
+    soundManager.setup({
+      url: '/swf/',
+      debugMode: false,
+      onready: function() {
+        self.sound = soundManager.createSound({
+          id: 'aSound',
+        url: '/files/salsalesson.mp3'
+        });
+        soundManager.load('aSound', {
+          onload: function(){
+            callback();
+          }
+        });
+
+      },
+      ontimeout: function() {
+        // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+      }
+    });
+  }
+};
 
 
 jQuery(document).ready(function(){
-    //jsapp.audio = jQuery('audio').get(0);
-    //jsapp.smDeploy();
-})
+  //jsapp.audio = jQuery('audio').get(0);
+  //jsapp.smDeploy();
+});
 
 
 //working steps
--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -3,103 +3,103 @@ if(!window.jsapp){
 }
 
 jsapp.view = {
-    stepmatrix: function(dance){
-        var guapeaMan = {
-            1: {'bottom': 'lfoot rfoot'},
-            2: {'bottom': 'lfoot', 'right': 'rfoot'},
-            3: {'right': 'lfoot rfoot'},
-            5: {'right': 'lfoot', 'top': 'rfoot'},
-            6: {'top': 'rfoot', 'left': 'lfoot'},
-            7: {'left': 'lfoot', 'bottom': 'rfoot'}
-        };
-        var guapeaWoman = {
-            1: {'bottom': 'lfoot rfoot'},
-            2: {'bottom': 'rfoot', 'left': 'lfoot'},
-            3: {'left': 'lfoot rfoot'},
-            5: {'left': 'rfoot', 'top': 'lfoot'},
-            6: {'top': 'lfoot', 'right': 'rfoot'},
-            7: {'right': 'rfoot', 'bottom': 'lfoot'}
-        };
-        var leftright = {
-            1: {'left': 'lfoot', 'center': 'rfoot fade'},
-            2: {'left': 'lfoot', 'center': 'rfoot'},
-            3: {'center': 'lfoot rfoot'},
-            5: {'right': 'rfoot', 'center': 'lfoot fade'},
-            6: {'right': 'rfoot', 'center': 'lfoot'},
-            7: {'center': 'lfoot rfoot'}
-        };
-        var frontback = {
-            1: {'top': 'lfoot', 'center': 'rfoot fade'},
-            2: {'top': 'lfoot', 'center': 'rfoot'},
-            3: {'center': 'lfoot rfoot'},
-            5: {'bottom': 'rfoot', 'center': 'lfoot fade'},
-            6: {'bottom': 'rfoot', 'center': 'lfoot'},
-            7: {'center': 'lfoot rfoot'}
-        };
-        var known = {
-            'guapea-man': guapeaMan,
-            'guapea-woman': guapeaWoman,
-            'leftright': leftright,
-            'frontback': frontback
-        };
-        return known[dance];
-    },
-    startPlay: function(){
-        var self = this;
-        /*
-        jsapp.steps = [
-            9.1, 9.45, 9.87, 10.6, 10.95, 11.33,
-            12.08, 12.47, 12.88, 13.58, 13.97, 14.34,
-            15.08, 15.48, 15.87, 16.57, 16.96, 17.32,
-            18.09, 18.47, 18.85, 19.57, 19.95, 20.34,
-            21.08, 21.46, 21.88, 22.57, 22.96, 23.34
-        ]*/
-        /*jsapp.steps = [
-            9378, 9848, 10214, 10815, 11233, 11598,
-            12434, 12800, 13140, 13923, 14211, 14655,
-            15464, 15726, 16144, 16901, 17319, 17737,
-            18416, 18808, 19226, 19905, 20245, 20611,
-            21394, 21812, 22230, 22831, 23249, 23641,
-            24477, 24842, 25156, 25861, 26227, 26645,
-            27350, 27768, 28134, 28839, 29257, 29675,
-            30354, 30746, 31164, 31843, 32261, 32627
-        ]*/
-        jsapp.steps = [
-            9456, 9874, 10266, 10945, 11285, 11703,
-            12487, 12852, 13192, 13871, 14289, 14655,
-            15360, 15778, 16144, 16980, 17267, 17685,
-            18469, 18887, 19174, 19958, 20376, 20663,
-            21394, 21812, 22178, 23014, 23353, 23719,
-            24477, 24842, 25260, 25940, 26358, 26645,
-            27429, 27847, 28264, 28865, 29283, 29701,
-            30407, 30824, 31164, 31948, 32261, 32784,
-            33463, 33855, 34273, 34952, 35370, 35762,
-            36441, 36780, 37198, 37878, 38269, 38687
+  stepmatrix: function(dance){
+    var guapeaMan = {
+      1: {'bottom': 'lfoot rfoot'},
+      2: {'bottom': 'lfoot', 'right': 'rfoot'},
+      3: {'right': 'lfoot rfoot'},
+      5: {'right': 'lfoot', 'top': 'rfoot'},
+      6: {'top': 'rfoot', 'left': 'lfoot'},
+      7: {'left': 'lfoot', 'bottom': 'rfoot'}
+    };
+    var guapeaWoman = {
+      1: {'bottom': 'lfoot rfoot'},
+      2: {'bottom': 'rfoot', 'left': 'lfoot'},
+      3: {'left': 'lfoot rfoot'},
+      5: {'left': 'rfoot', 'top': 'lfoot'},
+      6: {'top': 'lfoot', 'right': 'rfoot'},
+      7: {'right': 'rfoot', 'bottom': 'lfoot'}
+    };
+    var leftright = {
+      1: {'left': 'lfoot', 'center': 'rfoot fade'},
+      2: {'left': 'lfoot', 'center': 'rfoot'},
+      3: {'center': 'lfoot rfoot'},
+      5: {'right': 'rfoot', 'center': 'lfoot fade'},
+      6: {'right': 'rfoot', 'center': 'lfoot'},
+      7: {'center': 'lfoot rfoot'}
+    };
+    var frontback = {
+      1: {'top': 'lfoot', 'center': 'rfoot fade'},
+      2: {'top': 'lfoot', 'center': 'rfoot'},
+      3: {'center': 'lfoot rfoot'},
+      5: {'bottom': 'rfoot', 'center': 'lfoot fade'},
+      6: {'bottom': 'rfoot', 'center': 'lfoot'},
+      7: {'center': 'lfoot rfoot'}
+    };
+    var known = {
+      'guapea-man': guapeaMan,
+      'guapea-woman': guapeaWoman,
+      'leftright': leftright,
+      'frontback': frontback
+    };
+    return known[dance];
+  },
+  startPlay: function(){
+    var self = this;
+    /*
+       jsapp.steps = [
+       9.1, 9.45, 9.87, 10.6, 10.95, 11.33,
+       12.08, 12.47, 12.88, 13.58, 13.97, 14.34,
+       15.08, 15.48, 15.87, 16.57, 16.96, 17.32,
+       18.09, 18.47, 18.85, 19.57, 19.95, 20.34,
+       21.08, 21.46, 21.88, 22.57, 22.96, 23.34
+       ]*/
+    /*jsapp.steps = [
+      9378, 9848, 10214, 10815, 11233, 11598,
+      12434, 12800, 13140, 13923, 14211, 14655,
+      15464, 15726, 16144, 16901, 17319, 17737,
+      18416, 18808, 19226, 19905, 20245, 20611,
+      21394, 21812, 22230, 22831, 23249, 23641,
+      24477, 24842, 25156, 25861, 26227, 26645,
+      27350, 27768, 28134, 28839, 29257, 29675,
+      30354, 30746, 31164, 31843, 32261, 32627
+      ]*/
+    jsapp.steps = [
+      9456, 9874, 10266, 10945, 11285, 11703,
+      12487, 12852, 13192, 13871, 14289, 14655,
+      15360, 15778, 16144, 16980, 17267, 17685,
+      18469, 18887, 19174, 19958, 20376, 20663,
+      21394, 21812, 22178, 23014, 23353, 23719,
+      24477, 24842, 25260, 25940, 26358, 26645,
+      27429, 27847, 28264, 28865, 29283, 29701,
+      30407, 30824, 31164, 31948, 32261, 32784,
+      33463, 33855, 34273, 34952, 35370, 35762,
+      36441, 36780, 37198, 37878, 38269, 38687
         ];
-        jQuery('#wait')
-            .removeClass('alert-info')
-            .addClass('alert-success')
-            .text('Dance!');
-        jsapp.handlePlayback(jsapp.stepMap(), function(step) {
-            var self = jsapp.view;
-            jQuery('#stepdisplay .step').removeClass('lfoot rfoot fade');
-            self.showDance(step, 'guapea-man');
-            self.showDance(step, 'guapea-woman');
-            self.showDance(step, 'leftright');
-            self.showDance(step, 'frontback');
-        });
-    },
-    showDance: function(step, dance){
-        var stepmatrix = jsapp.view.stepmatrix(dance);
-        for(var pos in stepmatrix[step]){
-            var feet = stepmatrix[step][pos];
-            var square = jQuery('.' + pos, '#stepdisplay ' + '.' + dance);
-            square.addClass(feet);
-        }
+    jQuery('#wait')
+      .removeClass('alert-info')
+      .addClass('alert-success')
+      .text('Dance!');
+    jsapp.handlePlayback(jsapp.stepMap(), function(step) {
+      var self = jsapp.view;
+      jQuery('#stepdisplay .step').removeClass('lfoot rfoot fade');
+      self.showDance(step, 'guapea-man');
+      self.showDance(step, 'guapea-woman');
+      self.showDance(step, 'leftright');
+      self.showDance(step, 'frontback');
+    });
+  },
+  showDance: function(step, dance){
+    var stepmatrix = jsapp.view.stepmatrix(dance);
+    for(var pos in stepmatrix[step]){
+      var feet = stepmatrix[step][pos];
+      var square = jQuery('.' + pos, '#stepdisplay ' + '.' + dance);
+      square.addClass(feet);
     }
+  }
 };
 
 
 jQuery(document).ready(function(){
-    jsapp.smDeploy(jsapp.view.startPlay);
+  jsapp.smDeploy(jsapp.view.startPlay);
 });
