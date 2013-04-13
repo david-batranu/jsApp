9714f31a01bddc8dfb0e0b5bf7470acaad15b273
David Batranu <dbatranu@gmail.com>
2013-04-13 17:58:57 +0300

    Indenting

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,28 +1,28 @@
 if(!window.jsapp){
-    var jsapp = {"version": "1.0"};
+  var jsapp = {"version": "1.0"};
 }
 
 jsapp.edit = {
-    bindButtons: function(){
-        var self = this;
-        jQuery('#steps').on('click', function(){
-            var currentTime = jsapp.getCurrentTime();
-            jsapp.steps[jsapp.steps.length] = currentTime;
-            console.log(currentTime);
-        });
-        jQuery('#steps-clear').on('click', function(){
-            jsapp.steps = [];
-        })
-        jQuery('#generate').on('click', function(){
-            jsapp.start = jsapp.steps[0];
-            console.log(jsapp.stepMap());
-            jsapp.printSteps();
-            jsapp.restartPlay();
-        });
-    }
-}
+  bindButtons: function(){
+    var self = this;
+    jQuery('#steps').on('click', function(){
+      var currentTime = jsapp.getCurrentTime();
+      jsapp.steps[jsapp.steps.length] = currentTime;
+      console.log(currentTime);
+    });
+    jQuery('#steps-clear').on('click', function(){
+      jsapp.steps = [];
+    });
+    jQuery('#generate').on('click', function(){
+      jsapp.start = jsapp.steps[0];
+      console.log(jsapp.stepMap());
+      jsapp.printSteps();
+      jsapp.restartPlay();
+    });
+  }
+};
 
 
 jQuery(document).ready(function(){
-    jsapp.edit.bindButtons();
-})
+  jsapp.edit.bindButtons();
+});
